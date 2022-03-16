7eea28dd1278187f33192e643af328c1acbabff4
akshay2408 <akshaybabar2408@gmail.com>
2022-03-16 18:33:38 +0530

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import logo from './logo.svg';
 import MoviesList from "./MovieList/MoviesList";
 import { Routes, Route } from "react-router-dom";
 import MovieDetail from "./MovieDetail/MovieDetail";
